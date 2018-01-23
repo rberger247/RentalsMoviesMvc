68a555af66cba51eef0f62b6d3ffecb25ee03146
Rafael Berger <Rafiberger613@gmail.com>
2018-01-22 21:15:08 -0500

    Initial

---
+++ RentalsMoviesMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentalsMoviesMvc.Startup))]
+namespace RentalsMoviesMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
