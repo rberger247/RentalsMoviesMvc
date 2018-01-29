7a8701551d867d421ff241c6868a41c11f9d12f8
Rafael Berger <Rafiberger613@gmail.com>
2018-01-29 16:18:03 -0500

    Added controller and view for adding movies

---
+++ RentalsMoviesMvc/Controllers/MoviesController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RentalsMoviesMvc.Models;
+using RentalsMoviesMvc.ViewModels;
+
+namespace RentalsMoviesMvc.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+
+        private readonly RentalStoreEntities _context;
+        public MoviesController()
+        {
+            _context = new RentalStoreEntities();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ViewResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        
+        
+        
+        
+        public ActionResult Save(Movies movie)
+        {
+
+
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new MovieFormViewModel(movie)
+                {
+                    Genres = _context.Genres.ToList()
+                };
+
+                return View("MovieForm", viewModel);
+            }
+
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
+        }
+
+        }
+    }
+
+
